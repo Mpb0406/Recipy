553dc7025c9998f68ae4b18d89f72e3b2fccfe35
Mike Bolloskis <mpb0406@gmail.com>
2021-08-12 22:31:46 -0400

    Added Validation for Post Recipe Route

--- routes/api/recipe.js
+++ routes/api/recipe.js
@@ -7,8 +7,17 @@ const auth = require('../../middleware/auth');
 //@route    POST api/recipes
 //@desc     Post a recipe to user account
 //@access   Private
-router.post('/', (req, res) => {
-    res.send('Post a recipe')
+router.post('/', [auth, [
+    check('title', 'Recipe title is required').not().isEmpty(),
+    check('description', 'Recipe description is required').not().isEmpty(),
+    check('amount', 'Please enter an amount').not().isEmpty(),
+    check('unit', 'Measurement unit is required').not().isEmpty(),
+    check('item', 'Name of ingredient required').not().isEmpty(),
+    check('step', 'Please enter at least one procedure').not().isEmpty(),
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) res.status(400).json({ errors: errors.array() });
+    res.send('Post a recipe');
 });
 
 module.exports = router;
