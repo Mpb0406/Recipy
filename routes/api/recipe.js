567b92172ecd23a301ace66d65578e1290eca3af
Mpb0406 <mpb0406@gmail.com>
2021-08-13 22:28:35 -0400

    Add Recipe Route - Add Title and Description Complete

--- routes/api/recipe.js
+++ routes/api/recipe.js
@@ -1,23 +1,52 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Recipe = require('../../Models/Recipe');
-const { check, validationResult } = require('express-validator');
-const auth = require('../../middleware/auth');
+const Recipe = require("../../Models/Recipe");
+const { check, validationResult } = require("express-validator");
+const auth = require("../../middleware/auth");
 
 //@route    POST api/recipes
-//@desc     Post a recipe to user account
+//@desc     Post a recipe (title & description) to user account
 //@access   Private
-router.post('/', [auth, [
-    check('title', 'Recipe title is required').not().isEmpty(),
-    check('description', 'Recipe description is required').not().isEmpty(),
-    check('amount', 'Please enter an amount').not().isEmpty(),
-    check('unit', 'Measurement unit is required').not().isEmpty(),
-    check('item', 'Name of ingredient required').not().isEmpty(),
-    check('step', 'Please enter at least one procedure').not().isEmpty(),
-]], async (req, res) => {
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("title", "Recipe title is required").not().isEmpty(),
+      check("description", "Recipe description is required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) res.status(400).json({ errors: errors.array() });
-    res.send('Post a recipe');
-});
+
+    // Destructure all elements in model
+    const { title, description, ingredients, procedures } = req.body;
+
+    // Build a recipe object with all elements available in request
+    const recipeFields = {};
+    recipeFields.user = req.user.id;
+    if (title) recipeFields.title = title;
+    if (description) recipeFields.description = description;
+    if (ingredients) recipeFields.ingredients = ingredients;
+    if (procedures) recipeFields.procedures = procedures;
+
+    //Build Ingredients
+    const { amount, unit, item } = req.body;
+
+    recipeFields.ingredients = {};
+    recipeFields.ingredients.amount = amount;
+
+    // Assign recipe object to new Recipe model and save & return
+    try {
+      const recipe = new Recipe(recipeFields);
+      await recipe.save();
+      res.send(recipe);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
