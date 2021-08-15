bf094f78d78e8e0a7486363fb6e90728bcce7a82
Mike Bolloskis <mpb0406@gmail.com>
2021-08-15 17:58:21 -0400

    Added Update Recipe Route and Set-up Delete Recipe Route

--- routes/api/recipe.js
+++ routes/api/recipe.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Recipe = require("../../Models/Recipe");
 const { check, validationResult } = require("express-validator");
 const auth = require("../../middleware/auth");
-const { findById } = require("../../Models/Recipe");
+const { findById, findOneAndUpdate } = require("../../Models/Recipe");
 
 //@route    POST api/recipes
 //@desc     Post a recipe (title & description) to user account
@@ -64,13 +64,79 @@ router.post(
 );
 
 
-//@route    GET api/recipes/:id
+//@route    GET api/recipes/myrecipes
 //@desc     Get all recipes for a user
 //@access   Private
 router.get('/myrecipes', auth, async (req, res) => {
-  const userPosts = await Recipe.find({ 'user': req.user.id });
 
-  res.send(userPosts);
+  try {
+    const userPosts = await Recipe.find({ 'user': req.user.id });
+    res.send(userPosts);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server Error');
+  }
+});
+
+
+//@route    PUT api/recipes/:id
+//@desc     Update a recipe
+//@access   Private
+router.put('/:id', auth, async (req, res) => {
+  const recipe = await Recipe.findById(req.params.id);
+
+  const { title, description, ingredients, procedures, yield, preptime, cooktime, pairswith, tags } = req.body;
+
+  // Build a recipe object with all elements available in request
+  const recipeFields = {};
+  recipeFields.user = req.user.id;
+  if (title) recipeFields.title = title;
+  if (description) recipeFields.description = description;
+  if (ingredients) recipeFields.ingredients = ingredients;
+  if (procedures) recipeFields.procedures = procedures;
+  if (yield) recipeFields.yield = yield;
+  if (preptime) recipeFields.preptime = preptime;
+  if (cooktime) recipeFields.cooktime = cooktime;
+  if (pairswith) recipeFields.pairswith = pairswith;
+  if (tags) recipeFields.tags = tags;
+
+  //Grab Ingredients from Req
+  const recipeIngredients = req.body.ingredients;
+
+  //Map Over Each Ingredient Obj in Req and Push to Ingredients Array
+  recipeFields.ingredients = [];
+  recipeIngredients.map(ingredient => recipeFields.ingredients.push(ingredient));
+
+  //Grab Procedures from Req
+  const recipeProcedures = req.body.procedures;
+
+  //Map Over Each Ingredient Obj in Req and Push to Ingredients Array
+  recipeFields.procedures = [];
+  recipeProcedures.map(procedure => recipeFields.procedures.push(procedure));
+
+
+  try {
+    if (recipe) {
+      updatedRecipe = await Recipe.findOneAndUpdate({ user: req.user.id }, { $set: recipeFields }, { new: true, useFindAndModify: false })
+    }
+    res.send(updatedRecipe);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server Error');
+  }
+  if (recipe) {
+    updatedRecipe = await Recipe.findOneAndUpdate({ user: req.user.id }, { $set: recipeFields }, { new: true, useFindAndModify: false })
+  }
+  res.send(updatedRecipe);
+});
+
+
+//@route    DELETE api/recipes/:id
+//@desc     Delete a recipe
+//@access   Private
+router.delete('/:id', auth, async (req, res) => {
+  res.send('Delete Recipe');
 })
+
 
 module.exports = router;
