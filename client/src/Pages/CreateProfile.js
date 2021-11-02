b8a18fb5aba652558ae259cea067e0c649f3ccd2
Mike Bolloskis <mpb0406@gmail.com>
2021-11-01 20:54:25 -0400

    Added Some Markup and Styling to CreateProfile Page

--- client/src/Pages/CreateProfile.js
+++ client/src/Pages/CreateProfile.js
@@ -1,13 +1,56 @@
 import React from "react";
+import styled from "styled-components";
 
 const Createprofile = () => {
   return (
-    <div>
+    <StyledDiv>
       <h1>
-        Create <span>Profile Page</span>{" "}
+        Create <span>Your Profile</span>{" "}
       </h1>
-    </div>
+
+      <div className="profile-pic">
+        <div className="picture-container">
+          <i className="fas fa-user"></i>
+        </div>
+        <p>Add a profile picture</p>
+      </div>
+
+      <div className="bio-section">
+        <p>Tell us about yourself...</p>
+        <textarea
+          name=""
+          id=""
+          cols="100"
+          rows="5"
+          placeholder="Bio..."
+        ></textarea>
+      </div>
+    </StyledDiv>
   );
 };
+
+// Styled Components
+const StyledDiv = styled.div`
+  .profile-pic {
+    display: flex;
+    align-items: center;
+    margin-bottom: 5rem;
+    .picture-container {
+      height: 10rem;
+      width: 10rem;
+      background: #c4c4c4;
+      border-radius: 50%;
+      margin-right: 2rem;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      .fa-user {
+        color: #fff;
+        font-size: 5rem;
+      }
+    }
+  }
+`;
 
 export default Createprofile;
