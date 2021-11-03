404b03c8457ac901e45bc5db835d09fa9f6f3726
Mike Bolloskis <mpb0406@gmail.com>
2021-11-02 20:19:36 -0400

    Finished Styling Create Profile Form

--- client/src/Pages/CreateProfile.js
+++ client/src/Pages/CreateProfile.js
@@ -1,10 +1,11 @@
 import React from "react";
 import styled from "styled-components";
+import { MainButton } from "../Styles";
 
 const Createprofile = () => {
   return (
-    <StyledDiv>
-      <h1>
+    <StyledForm>
+      <h1 className="title">
         Create <span>Your Profile</span>{" "}
       </h1>
 
@@ -12,7 +13,10 @@ const Createprofile = () => {
         <div className="picture-container">
           <i className="fas fa-user"></i>
         </div>
-        <p>Add a profile picture</p>
+        <div className="file-upload">
+          <p>Add a profile picture</p>
+          <input type="file" />
+        </div>
       </div>
 
       <div className="bio-section">
@@ -25,16 +29,27 @@ const Createprofile = () => {
           placeholder="Bio..."
         ></textarea>
       </div>
-    </StyledDiv>
+      <MainButton>Submit</MainButton>
+    </StyledForm>
   );
 };
 
 // Styled Components
-const StyledDiv = styled.div`
+const StyledForm = styled.form`
+  width: 90%;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .title {
+    margin-bottom: 2rem;
+  }
+
   .profile-pic {
     display: flex;
     align-items: center;
-    margin-bottom: 5rem;
+    margin-bottom: 2rem;
     .picture-container {
       height: 10rem;
       width: 10rem;
@@ -50,6 +65,13 @@ const StyledDiv = styled.div`
         font-size: 5rem;
       }
     }
+  }
+  p {
+    margin-bottom: 0.5rem;
+  }
+  button {
+    margin: 2rem;
+    align-self: center;
   }
 `;
 
